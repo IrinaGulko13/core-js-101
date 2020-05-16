d5a0e726ae84aa1dec72932421dbfeb1a201722d
Iryna Hulko <irinagulko13@gmail.com>
2020-05-16 12:57:47 +0300

    feat:promises-tasks

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -56,8 +56,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array).then((values) => values);
 }
 
 /**
@@ -79,8 +79,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array).then((value) => value);
 }
 
 /**
@@ -100,8 +100,15 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  return array.reduce(
+    (chainOfPromises, currentPromise) => chainOfPromises.then(
+      (chainResult) => currentPromise.then(
+        (promiseResult) => [...chainResult, promiseResult],
+        () => chainResult,
+      ),
+    ), Promise.resolve([]),
+  ).then((res) => res.reduce(action));
 }
 
 module.exports = {
